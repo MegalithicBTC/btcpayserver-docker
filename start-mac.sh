c2ba8b8450417f372f837c382d5e513e8776f9cb
Megalithic.me <info@megalithic.me>
2025-05-11 17:55:55 -0700

    m

--- start-mac.sh
+++ start-mac.sh
@@ -1,5 +1,8 @@
 docker compose \
   -f Generated/docker-compose-megalith-dev.yml \
+  down
+docker compose \
+  -f Generated/docker-compose-megalith-dev.yml \
   up
   #\ we commented it out
   #--scale btcpayserver=0   
